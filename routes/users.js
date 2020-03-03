56d0ca0694ef43571d9945199b738f82ff3f276f
emmavanninen <emmavanninen@gmail.com>
2020-03-03 15:47:32 -0500

    add log in login

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,46 @@
-var express = require("express");
-var router = express.Router();
-const passport = require("passport");
-const userController = require("../controllers/userController");
-const auth = require("../middleware/auth");
+var express = require('express')
+var router = express.Router()
+const passport = require('passport')
+const userController = require('../controllers/userController')
+const auth = require('../middleware/auth')
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
-});
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource')
+})
 
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   userController
     .registerNewUser(req.body)
     .then(user => {
-      res.send(user);
+      res.send(user)
     })
     .catch(err => {
-      res.status(400).json(err);
-    });
-});
+      res.status(400).json(err)
+    })
+})
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   userController
     .userLogin(req.body)
     .then(user => {
-      res.send(user);
+      res.send(user)
     })
     .catch(err => {
-      res.status(400).json(err);
-    });
-});
+      console.log(err)
+      res.status(400).json(err)
+    })
+})
 
-router.post("/logout", (req, res) => {
-
+router.post('/logout', (req, res) => {
   userController
     .userLogout(req.body)
     .then(result => {
       res.send(result)
     })
     .catch(err => {
-      res.status(400).json(err);
-    });
-});
+      res.status(400).json(err)
+    })
+})
 
-module.exports = router;
+module.exports = router
